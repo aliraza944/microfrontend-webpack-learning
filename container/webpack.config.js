27774adcebc4a194236902ed5c6686958f29daf9
aliraza944 <alisabri022@gmail.com>
2023-01-21 23:11:20 +0500

    Added the module federation plugin for container

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,8 +1,17 @@
 const htmlPlugin = require("html-webpack-plugin");
+const moduleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 module.exports = {
   mode: "development",
   devServer: {
     port: 8080,
   },
-  plugins: [new htmlPlugin({ template: "./public/index.html" })],
+  plugins: [
+    new moduleFederationPlugin({
+      name: "container",
+      remotes: {
+        products: "products@http://localhost:8081/remoteEntry.js",
+      },
+    }),
+    new htmlPlugin({ template: "./public/index.html" }),
+  ],
 };
