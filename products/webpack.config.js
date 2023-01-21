8c758e92b88e7d7083960d01cb9b9ea9703ea45a
aliraza944 <alisabri022@gmail.com>
2023-01-21 23:06:50 +0500

    Added the module fedration plugin and configured it with products

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,8 +1,18 @@
 const htmlPlugin = require("html-webpack-plugin");
+const moduleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 module.exports = {
   mode: "development",
   devServer: {
     port: 8081,
   },
-  plugins: [new htmlPlugin({ template: "./public/index.html" })],
+  plugins: [
+    new moduleFederationPlugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+    }),
+    new htmlPlugin({ template: "./public/index.html" }),
+  ],
 };
