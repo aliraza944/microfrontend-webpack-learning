d97635214dfbd584699e58d0f46d2eca074d8e44
aliraza944 <alisabri022@gmail.com>
2023-01-31 22:05:21 +0500

    Async downloaded the faker module for indivsual cart and products port

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,1 @@
-import faker from "faker";
-
-let products = "";
-
-for (let i = 0; i < 10; i++) {
-  products += `
-        <div>${faker.commerce.productName()}</div>
-    `;
-}
-document.querySelector("#dev-products").innerHTML = products;
+import("./bootstrap");
