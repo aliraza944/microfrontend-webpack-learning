66af7c8d202ebe9ea551436d522b0e6db345e1e2
aliraza944 <alisabri022@gmail.com>
2023-01-31 22:00:30 +0500

    Added the shared faker to the webpack config of cart and product

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
       exposes: {
         "./cartShow": "./src/index",
       },
+      shared: ["faker"],
     }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
